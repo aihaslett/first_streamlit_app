c8a1fddc9385f4b4246382d806c6faf2afa1a9ac
aihaslett <60903278+aihaslett@users.noreply.github.com>
2023-05-27 15:14:12 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My parents'' new healthy diner')
+streamlit.header('Breakfast menu')
+streamlit.text('Blueberry pancakes')
+streamlit.text('Banana and honey waffles')
+streamlit.text('A lettuce leaf')
+
+                
